cc1dc619239553098b5cc3f6cb25f5d16543af31
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-03-04 21:10:46 +0800

    new file:   .tamagui/tamagui-components.config.cjs 	new file:   .tamagui/tamagui.config.cjs 	new file:   .tamagui/tamagui.config.json 	modified:   App.tsx 	new file:   metro.config.js 	modified:   package.json 	new file:   pages/test.tsx 	new file:   tamagui-web.css 	new file:   tamagui.config.ts 	modified:   tsconfig.json 	modified:   yarn.lock

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
-import SendTest from "./pages/send";
+// optional but recommended CSS reset:
 
-export default function App() {
-  return (
-    <SendTest />
-  )
+import { TamaguiProvider, View } from '@tamagui/core'
+import tamaguiConfig from './tamagui.config'
+import '@tamagui/core/reset.css'
+import Test from './pages/test'
 
+type Conf = typeof tamaguiConfig
+declare module '@tamagui/core' {
+  interface TamaguiCustomConfig extends Conf { }
 }
+
+export default () => {
+  return (
+    <TamaguiProvider config={tamaguiConfig}>
+      <Test />
+    </TamaguiProvider>
+  );
+};
---
+++ pages/test.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from 'react-native';
+import {Button} from 'tamagui';
+
+export default function Test() {
+    return (
+        <SafeAreaView>
+            <Button>Hello world</Button>
+        </SafeAreaView>
+    );
+}
