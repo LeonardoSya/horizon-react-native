795d76b81dd160034f127b57b5de44e95526f4fd
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-03-13 20:50:55 +0800

    register

--- pages/user/register.tsx
+++ pages/user/register.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react'
-import { View, TextInput, ActivityIndicator, Alert, Button } from 'react-native'
-import { Text } from '@rneui/themed'
+import { View, TextInput, ActivityIndicator, Alert } from 'react-native'
+import { Text, Button } from '@rneui/themed'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 import { registerUser } from '@/services/user-services'
+import { styles } from '@/assets/styles/global'
+import { userStyles } from '@/assets/styles/user-styles'
 
 // 表单校验
 const RegisterSchema = Yup.object().shape({
-    uername: Yup.string().required('账号为必填'),
-    email: Yup.string().email('Invalid email').required('邮箱为必填'),
+    username: Yup.string().required('账号为必填'),
+    email: Yup.string().email('邮箱格式不合法').required('邮箱为必填'),
     password: Yup.string().min(6, '密码应长于6个字符').required('密码为必填'),
 })
 
@@ -39,17 +41,9 @@ const RegisterComponent = () => {
             onSubmit={handleRegistration}  // 表单数据通过校验后执行 通常用于处理表单数据的提交(如发送api请求)
         >
             {({ handleChange, handleBlur, handleSubmit, values, errors, touched }) => (
-                <View>
+                <View style={styles.container}>
                     <TextInput
-                        onChangeText={handleChange('username')}  // 当username输入框内容变化时候，handleChange函数调用  handleChange函数用于更新username值
-                        onBlur={handleBlur('username')}  // 输入框失去焦点时 handleBlur调用(通常处理表单验证)
-                        value={values.username}  // 输入框的值绑定到 Formik的username
-                        placeholder='Username'
-                    />
-                    {/* 如果username字段已被触摸过(失焦)并存在验证错误 就显示错误信息 */}
-                    {touched.username && errors.username && <Text>{errors.username}</Text>}
-
-                    <TextInput
+                        style={userStyles.textInput}
                         onChangeText={handleChange('email')}
                         onBlur={handleBlur('email')}
                         value={values.email}
@@ -59,6 +53,17 @@ const RegisterComponent = () => {
                     {touched.email && errors.email && <Text>{errors.email}</Text>}
 
                     <TextInput
+                        style={userStyles.textInput}
+                        onChangeText={handleChange('username')}  // 当username输入框内容变化时候，handleChange函数调用  handleChange函数用于更新username值
+                        onBlur={handleBlur('username')}  // 输入框失去焦点时 handleBlur调用(通常处理表单验证)
+                        value={values.username}  // 输入框的值绑定到 Formik的username
+                        placeholder='Username'
+                    />
+                    {/* 如果username字段已被触摸过(失焦)并存在验证错误 就显示错误信息 */}
+                    {touched.username && errors.username && <Text>{errors.username}</Text>}
+
+                    <TextInput
+                        style={userStyles.textInput}
                         onChangeText={handleChange('password')}
                         onBlur={handleBlur('password')}
                         value={values.password}
@@ -67,7 +72,10 @@ const RegisterComponent = () => {
                     />
                     {touched.password && errors.password && <Text>{errors.password}</Text>}
 
-                    <Button onPress={handleSubmit} title="立即注册" />
+                    <Button
+                        onPress={handleSubmit}
+                        title="立即注册"
+                    />
                 </View>
             )}
         </Formik>
