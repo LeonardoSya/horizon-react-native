657b89ebbe051ebcc051c9179a4b757548088575
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-03-13 19:57:27 +0800

    modified:   App.tsx 	new file:   api/api.ts 	modified:   package.json 	new file:   pages/user/register.tsx 	modified:   pages/user/screen1.tsx 	modified:   pages/user/user.tsx 	new file:   services/user-services.ts 	modified:   yarn.lock

--- pages/user/user.tsx
+++ pages/user/user.tsx
@@ -1,51 +1,10 @@
-import { useCallback } from 'react';
-import { StackScreenProps } from '@react-navigation/stack';
-import { useFocusEffect } from '@react-navigation/native';
-import { RootParamList } from '../../router/root';
-import { Text, Avatar, Badge, Button } from "@rneui/themed";
-import { View, ScrollView } from 'react-native';
-import { styles } from '@/assets/styles/global';
-
-type UserProps = StackScreenProps<RootParamList, 'User'>;
+import RegisterComponent from "./register"
 
 
-const User = ({ navigation, route }: UserProps) => {
-
-    useFocusEffect(
-        useCallback(() => {
-            console.log('User screen is focused');
-
-            return () => {
-                console.log('User screen is unfocused');
-            }
-        }, [])
-    );
-
+const User = () => {
     return (
-        <View style={styles.container}>
-            <Avatar
-                size={32}
-                rounded
-                source={{ uri: "https://randomuser.me/api/portraits/men/36.jpg" }}
-            >
-                <Badge
-                    status="error"
-                    containerStyle={{
-                        position: 'absolute',
-                        top: 0,
-                        right: 0,
-                    }}
-                />
-            </Avatar>
-            <Text>
-                User
-            </Text>
-            <Button
-                onPress={() => { navigation.push('Screen1',) }}
-                title='to Screen1'
-            />
-        </View>
-    );
+        <RegisterComponent />
+    )
 }
 
-export default User;
+export default User
