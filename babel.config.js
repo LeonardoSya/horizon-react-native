5c611ef128529b396359a98d516ca92963317a4d
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-05-07 18:15:48 +0800

    enhance babel.config.js & package.json

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,19 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            tests: ['./tests/'],
+            '@components': './components'
+          }
+        }
+      ],
+      'react-native-reanimated/plugin'
+    ]
   };
 };
