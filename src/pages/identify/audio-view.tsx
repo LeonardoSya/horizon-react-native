2078b7aa4ce1d7715b67d34d9e93c5e8d79e92ef
Zhang YiYang <120240967+LeonardoSya@users.noreply.github.com>
2024-07-18 17:17:12 +0800

    feat: :lipstick: rotate image and set play interval

--- src/pages/identify/audio-view.tsx
+++ src/pages/identify/audio-view.tsx
@@ -1,16 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { View, StyleSheet, Pressable, Image } from 'react-native'
 import { Audio } from 'expo-av'
 import { RFValue } from 'react-native-responsive-fontsize'
 import { Text } from '@rneui/base'
 import { Entypo, FontAwesome } from '@expo/vector-icons'
 import AnimatedPressable from '@/components/animated-pressable'
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withRepeat,
+  Easing,
+  cancelAnimation,
+} from 'react-native-reanimated'
 
 const AudioView = () => {
   const [recording, setRecording] = useState<Audio.Recording | undefined>()
   const [permissionResponse, requestPermission] = Audio.usePermissions()
   const [uri, setUri] = useState<string>('')
   const [sound, setSound] = useState<Audio.Sound | null>(null)
+  const [recordingDuration, setRecordingDuration] = useState<number>(0)
+  const [soundDuration, setSoundDuration] = useState<number>(0)
+  const [soundPosition, setSoundPosition] = useState<number>(0)
+  const rotation = useSharedValue(0)
+  const currentRotation = useSharedValue(0)
 
   const startRecording = async () => {
     try {
@@ -28,6 +41,16 @@ const AudioView = () => {
         Audio.RecordingOptionsPresets.HIGH_QUALITY,
       )
       setRecording(recording)
+
+      const recordingInterval = setInterval(() => {
+        setRecordingDuration(prev => prev + 1)
+      }, 1000)
+      recording.setOnRecordingStatusUpdate(status => {
+        if (!status.isRecording) {
+          clearInterval(recordingInterval)
+        }
+      })
+      startImageRotation()
       console.log('Recording started')
     } catch (error) {
       console.error('Failed to start recording', error)
@@ -39,10 +62,12 @@ const AudioView = () => {
     if (recording) {
       console.log('Stopping recording..')
       setRecording(undefined)
+      setRecordingDuration(0)
       await recording.stopAndUnloadAsync()
       await Audio.setAudioModeAsync({ allowsRecordingIOS: false })
       const uri = recording.getURI()
       setUri(uri || '')
+      stopImageRotation()
       console.log('Recording stopped and stored at ', uri)
     }
   }
@@ -52,8 +77,23 @@ const AudioView = () => {
       console.log('Loading sound..')
       const { sound } = await Audio.Sound.createAsync({ uri })
       setSound(sound)
+      sound.setOnPlaybackStatusUpdate(status => {
+        if (status.isLoaded) {
+          updateStatus({
+            didJustFinish: status.didJustFinish,
+            positionMillis: status.positionMillis,
+          })
+        }
+      })
+
+      const status = await sound.getStatusAsync()
+      if (status.isLoaded) {
+        setSoundDuration(status.durationMillis || 0)
+      }
       console.log('Playing sound..')
       await sound.playAsync()
+
+      startImageRotation()
     }
   }
 
@@ -61,7 +101,20 @@ const AudioView = () => {
     if (sound) {
       console.log('Stopping sound..')
       await sound.stopAsync()
+      await sound.unloadAsync()
       setSound(null)
+      setSoundPosition(0)
+      stopImageRotation()
+    }
+  }
+
+  const updateStatus = (status: { didJustFinish: boolean, positionMillis: number }) => {
+    if (status.didJustFinish) {
+      setSound(null)
+      setSoundPosition(0)
+      stopImageRotation()
+    } else {
+      setSoundPosition(status.positionMillis || 0)
     }
   }
 
@@ -70,21 +123,52 @@ const AudioView = () => {
       ? () => {
           console.log('Unloading Sound..')
           sound.unloadAsync()
+          recording?.stopAndUnloadAsync()
         }
       : undefined
   }, [sound])
 
+  const formatTime = milliseconds => {
+    const minutes = Math.floor(milliseconds / 60000)
+    const seconds = Math.floor((milliseconds % 60000) / 1000)
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`
+  }
+
+  const startImageRotation = () => {
+    rotation.value = withRepeat(
+      withTiming(currentRotation.value + 360, {
+        duration: 10000,
+        easing: Easing.linear,
+      }),
+      -1, // 无限循环
+    )
+  }
+
+  const stopImageRotation = () => {
+    cancelAnimation(rotation)
+    currentRotation.value = rotation.value % 360
+  }
+
+  const rotateAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ rotate: `${rotation.value}deg` }],
+    }
+  })
+
   return (
     <View style={styles.container}>
-      <Image
-        style={{
-          width: RFValue(300),
-          height: RFValue(300),
-          shadowColor: 'rgb(124,145,146)',
-          shadowRadius: 16,
-          shadowOffset: { width: 0, height: 0 },
-          shadowOpacity: 0.6,
-        }}
+      <Animated.Image
+        style={[
+          {
+            width: RFValue(300),
+            height: RFValue(300),
+            shadowColor: 'rgb(124,145,146)',
+            shadowRadius: 16,
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 0.6,
+          },
+          rotateAnimatedStyle,
+        ]}
         source={require('../../../assets/audio-player.png')}
       />
       <View style={{ flexDirection: 'row', gap: RFValue(46) }}>
@@ -108,7 +192,7 @@ const AudioView = () => {
             />
           </AnimatedPressable>
           <Text style={{ fontSize: RFValue(10), color: '#ffffffd9' }}>
-            {recording ? '正在录音' : '录音'}
+            {recording ? formatTime(recordingDuration * 1000) : '录音'}
           </Text>
         </View>
 
@@ -146,7 +230,7 @@ const AudioView = () => {
             <Entypo size={RFValue(28)} name='controller-play' color={sound ? '#407f79' : '#fff'} />
           </AnimatedPressable>
           <Text style={{ fontSize: RFValue(10), color: '#ffffffd9' }}>
-            {sound ? '正在播放' : '播放'}
+            {sound ? formatTime(soundPosition - soundDuration) : '播放'}
           </Text>
         </View>
       </View>
